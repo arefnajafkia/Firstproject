5cb4cf4064f94a19f67383f66724ac209b604cd7
aref najafkia <arefnajafkia294@gmail.com>
2022-04-09 23:39:42 +0430

    modified page2.py

--- page.py/page2/page2.py
+++ page.py/page2/page2.py
@@ -45,6 +45,22 @@ print (' شما' ,diff,' داريد ')
 print ( 'You are ',diff,'years old.' )
 print ('--------')
 
+print('-'*30)
+# tarekh tavalod and rooz 
+Month = [ 'Farvardin','Ordibehesht', 'Khordad','Tir', 'Mordad', 'Shahrivar', 'Mehr', 'Aban', 'Azar', 'Day', 'Bahman', 'Esfand']
+
+Y = int(input('\nEnter year:'))
+M = int(input('\nEnter Month:'))
+D = int(input('\nEnter Day:'))
+
+end = ['st' ,  'nd' ,  'rd'] + 17*['th'] + [ 'st' , 'nd' , 'rd'] +7 * ['th'] + ['st']
+
+m_index = M-1
+d_index = D-1
+
+print (D, end[d_index] ,   ' ' ,  Month [m_index] ,   ' ' ,  Y)
+
+print('-'*30)
 
 
 # ميپرسم بازهم سوال داريد
