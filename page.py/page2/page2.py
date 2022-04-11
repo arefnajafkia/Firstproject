1d39678d933f28250d96d5ed48e8e3ecf9bec946
aref najafkia <arefnajafkia294@gmail.com>
2022-04-12 01:58:06 +0430

    modified:   page.py/page2/page2.py

--- page.py/page2/page2.py
+++ page.py/page2/page2.py
@@ -14,31 +14,26 @@ mi = str(now.minute)
 ss = str(now.second)
 print (mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
 
-print ('-'*20)
-
 # نوشتن تاربخ به ماه وسال وساعت
+print ('-'*20)
 import datetime as dt
 today_date = dt.date.today()
 print(today_date.strftime("The Current Date is :\n\n%A %B %d, %Y"))
 
 print ('^'*20)
-
+g = input ( "شمامردهستيد يازن؟ What is your gender, Mr.or Mrs.?")
 name = input ("  نام ونام خانوادگي شما please enter your name : ")
-
 birth_year = input ("متولد چه سالي هستي birth year: ")
 age = 1401 - int (birth_year)
 city = input (" متولد کدام شهريد Born in which city :")
+
 print('-'*30)
-
-print ( '!هستيد',city,'شما',age,'داريد وازشهر',name,'خوش آمديد')
-
-print ( "Welcome ",name, "you are" ,age, "years old  and You were born in  " ,city , "!" )
-
+print ( '!هستيد',city,'شما',age,'داريد وازشهر',g ,name,'خوش آمديد')
+print ( "Welcome ",g ,name, "you are" ,age, "years old  and You were born in  " ,city , "!" )
 print('-'*30)
 
 # tarekh tavalod and rooz 
 Month = [ 'Farvardin','Ordibehesht', 'Khordad','Tir', 'Mordad', 'Shahrivar', 'Mehr', 'Aban', 'Azar', 'Day', 'Bahman', 'Esfand']
-
 Y = birth_year
 M = int(input('متولد چه ماهي هستي\nEnter Month :'))
 D = int(input('متولد چه روزي هستي\nEnter Day :'))
@@ -49,7 +44,6 @@ m_index = M-1
 d_index = D-1
 
 print('-'*30)
+print (  'شما',g ,name,'\nYou are ',g ,name ,)
 print ('You were born ' ,D, end[d_index] ,   ' ' ,  Month [m_index] ,   ' ' ,  Y )
-#print ( 'You were born in the city',S,)
-#print ('you are', age ,'years old.')
 print('-'*30)
